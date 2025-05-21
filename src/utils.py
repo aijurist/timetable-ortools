f6d2c665be480a97e7cdd4237d5d4388fba52295
Shanthosh S <shanthosh811@gmail.com>
2025-05-21 08:26:06 +0530

    added folder structure

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,9 @@
 # add basic utility functions such as preprocessing data
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv(r'C:\Users\ShanthoshS\OneDrive\Desktop\timetable_scheduler\data\rooms.csv', encoding="utf-8", on_bad_lines='skip')
+
+count = df[df['block'] == 'Lab Block'].shape[0]
+print(count)
